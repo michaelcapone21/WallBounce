9d8ec956d5fe3ecce2541a0e5d1cb96a538c2e91
Michael Capone <michael.capone21@gmail.com>
2015-12-10 19:53:29 -0500

    "Added tank drive method to DriveTrain"

--- src/org/usfirst/frc/team4761/robot/RobotMap.java
+++ src/org/usfirst/frc/team4761/robot/RobotMap.java
@@ -1,4 +1,8 @@
 package org.usfirst.frc.team4761.robot;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Victor;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -6,13 +10,11 @@ package org.usfirst.frc.team4761.robot;
  * floating around.
  */
 public class RobotMap {
-    // For example to map the left and right motors, you could define the
-    // following variables to use with your drivetrain subsystem.
-    // public static int leftMotor = 1;
-    // public static int rightMotor = 2;
-    
-    // If you are using multiple modules, make sure to define both the port
-    // number and the module. For example you with a rangefinder:
-    // public static int rangefinderPort = 1;
-    // public static int rangefinderModule = 1;
+
+	public static Victor frontLeft = new Victor(1);
+	public static Victor frontRight = new Victor(2);
+	public static Victor rearLeft = new Victor (3);
+	public static Victor rearRight = new Victor (4); 
+	public static RobotDrive myDrive = new RobotDrive(frontLeft, frontRight, rearLeft, rearRight);
+	public static RobotDrive robotDrive = new RobotDrive(1, 2, 3, 4);
 }
---
+++ src/org/usfirst/frc/team4761/robot/subsystems/DriveTrain.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc.team4761.robot.subsystems;
+
+import org.usfirst.frc.team4761.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class DriveTrain extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+    }
+    public void stop() {
+    	RobotMap.robotDrive.tankDrive( 0.0, 0.0) ;
+    }
+    public void tankDrive(double arg1, double arg2) {
+    	RobotMap.robotDrive.tankDrive(arg1, arg2);
+    }
+}
+
